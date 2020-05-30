2eb971ac3aecfc654570b17c51b448e144a7f048
GON <hutabasuzukiryu@live.jp>
2020-05-30 22:10:31 +0900

    fix: settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,12 +15,12 @@
 #   end
 # end
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'localhost:3000'
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#       credentials: true
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:3000'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
