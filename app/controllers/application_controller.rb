cbc910438431d7bf1bc1f60e3bbc84e7311d8709
GON <hutabasuzukiryu@live.jp>
2020-06-06 01:05:07 +0900

    add: rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,16 @@ class ApplicationController < ActionController::API
 
   def health_check
     users = User.count
-    render json: {"message": "success", "users": users}
+    render json: { "message": 'success', "users": users }
   end
 
   protected
+
   def authenticate
-    authenticate_or_request_with_http_token do |token, options|
+    authenticate_or_request_with_http_token do |token, _options|
       auth = AuthenticateToken.find_by(token: token)
 
-      !auth.nil? && auth.active && auth.created_at > Time.now - 1.hour
+      !auth.nil? && auth.active && auth.created_at > Time.zone.now - 1.hour
     end
   end
 end
