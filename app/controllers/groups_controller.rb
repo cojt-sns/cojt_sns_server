de6cf048b52350ae8a6587d27b73e625b99d131d
uutann <uutann9916@gmail.com>
2020-06-07 02:15:23 +0900

    group_apiのindexとshowを実装してみた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,17 +3,20 @@ class GroupsController < ApplicationController
 
   # get /groups
   def index
-    render json: {
-      "id": 1,
-      "twitter_traceability": false,
-      "questions": [
-        "スマブラのプレイ時間は?"
-      ],
-      "introduction": false,
-      "tags": [
-        0
-      ]
-    }
+    if params[:Tagid].nil?
+      groupSearchResult=Group.find_by(public: true)
+      render json:groupSearchResult
+      return 
+    end
+
+    tag = Tag.find_by(id: params[:Tagid])
+
+    if tag.nil?
+      render json:{ "code": 402, "message": "該当するタグが存在しません。"}, status: 402
+      return 
+    end
+    groupSearchResult=tag.groups
+    render json:groupSearchResult
   end
 
   # post /groups
@@ -37,17 +40,24 @@ class GroupsController < ApplicationController
 
   # get /groups/{id}
   def show
-    render json: {
-      "id": 1,
-      "twitter_traceability": false,
-      "questions": [
-        "スマブラのプレイ時間は?"
-      ],
-      "introduction": false,
-      "tags": [
-        0
-      ]
-    }
+    group = Group.find_by(id: params[:id])
+    
+    if !group.public?
+      render json: { "code": 403, "message": "privateグループのため、見れません"}, status: 403
+      return 
+    end
+    if group.nil?
+      render json: { "code": 404, "message": "該当するグループが存在しません。"}, status: 404
+      return 
+    end
+    render json:group
+    # render json: {
+    #   "id": @group.id,
+    #   "twitter_traceability": @group.twitter_traceability,
+    #   "questions": @group.questions,
+    #   "introduction": @group.introduction,
+    #   "tags": @group.tags
+    # }
   end
 
   # put /groups/{id}
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   post 'auth/login'
   post 'auth/logout'
+  
+  get 'groups/', to: 'groups#index'
+  post '/groups', to: 'groups#create'
 
+  get 'groups/:id', to: 'groups#show'
+  put 'groups/:id', to: 'groups#update'
   resources :groups, only: [:index, :create, :show, :update]
 end
