1b736f9d080b49a51d73fe967dda5d2c1612954b
GON <hutabasuzukiryu@live.jp>
2020-06-08 02:05:28 +0900

    fix: url

--- entrypoint.stg.sh
+++ entrypoint.stg.sh
@@ -1,3 +1,3 @@
 # rake db:migrate db:create db:seed RAILS_ENV=production
-certbot certonly -n -m "$EMAIL" --standalone --agree-tos -d stg.cojt-sns.work
+certbot certonly -n -m "$EMAIL" --standalone --agree-tos -d api.stg.cojt-sns.work
 rails server -e production
