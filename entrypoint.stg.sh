eed6d8e31aca3ffbc0b633e9deb10d2c971759f2
GON <hutabasuzukiryu@live.jp>
2020-06-08 02:33:46 +0900

    fix

--- entrypoint.stg.sh
+++ entrypoint.stg.sh
@@ -5,4 +5,4 @@ DIR="/etc/letsencrypt/live/api.stg.cojt-sns.work"
 if [ ! -d $DIR ];then
 certbot certonly -n -m "$EMAIL" --standalone --agree-tos -d api.stg.cojt-sns.work
 fi
-rails server -d -e production
+rails server -e production
