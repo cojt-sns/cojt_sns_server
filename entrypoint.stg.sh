40b22b5351759c46cf7f812b8a6ad0fbb2eeb667
GON <hutabasuzukiryu@live.jp>
2020-06-08 01:50:55 +0900

    fix: entrypoint.sh

--- entrypoint.stg.sh
+++ entrypoint.stg.sh
@@ -1,3 +1,3 @@
 # rake db:migrate db:create db:seed RAILS_ENV=production
 certbot certonly -n -m "$EMAIL" --standalone --agree-tos -d stg.cojt-sns.work
-rails server -d -e production
+rails server -e production
