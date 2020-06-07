df49af28978acdd512918053cc4113154f740c79
GON <hutabasuzukiryu@live.jp>
2020-06-08 05:47:03 +0900

    wip

--- config/puma.rb
+++ config/puma.rb
@@ -39,8 +39,8 @@ plugin :tmp_restart
 
 if ENV.fetch('RAILS_ENV') { 'development' } == 'production'
   ssl_bind '0.0.0.0', '4000', {
-    key: '/opt/app/privkey.pem',
-    cert: '/opt/app/fullchain.pem',
+    key: '/etc/letsencrypt/live/stg2.cojt-sns.work/privkey.pem',
+    cert: '/etc/letsencrypt/live/stg2.cojt-sns.work/fullchain.pem',
     verify_mode: 'none'
   }
 end
