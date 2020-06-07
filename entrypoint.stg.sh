7a3749776d4d424cec479658fd8e0e21cd4a0f55
GON <hutabasuzukiryu@live.jp>
2020-06-08 04:57:35 +0900

    wip

--- entrypoint.stg.sh
+++ entrypoint.stg.sh
@@ -5,6 +5,7 @@ DIR="/etc/letsencrypt/live/stg2.cojt-sns.work"
 if [ ! -d $DIR ];then
 certbot certonly -n -m "$EMAIL" --standalone --agree-tos -d stg2.cojt-sns.work
 fi
+cp $DIR/* .
 ls $DIR
 chmod -R 744 $DIR
 rails server -e production
