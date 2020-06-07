a7e3786bc79ec31d37eda0306abefb40406b387e
GON <hutabasuzukiryu@live.jp>
2020-06-08 02:20:52 +0900

    fix: entrypoint

--- entrypoint.stg.sh
+++ entrypoint.stg.sh
@@ -1,3 +1,8 @@
 # rake db:migrate db:create db:seed RAILS_ENV=production
+
+DIR="/etc/letsencrypt/live/api.stg.cojt-sns.work"
+
+if [ ! -d $DIR ];then
 certbot certonly -n -m "$EMAIL" --standalone --agree-tos -d api.stg.cojt-sns.work
-rails server -e production
+fi
+rails server -d -e production
