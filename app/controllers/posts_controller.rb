415d14bbaa64169fb2a45bbc83232930df38b111
uutann <uutann9916@gmail.com>
2020-06-11 23:21:29 +0900

    post_apiの実装 コメントください gemfileも変わってしまっています。お手を煩わせてしまったら、申し訳ありません。

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,67 @@
+class PostsController < ApplicationController
+  before_action :authenticate, only: %i(show update destroy)
+  def new
+
+  end
+
+  # get /posts/{id}
+  def show
+    if params[:id].nil? || params[:id] =~ /[^0-9]+/
+      render json: { code: '400', message: 'Bad Request' }, status: :bad_request
+      return
+    end
+    
+    post=Post.find_by(id:params[:id])
+
+    unless post.group.public
+      render json: { "code": 403, "message": 'privateグループの投稿なので、見れません' }, status: :forbidden
+      return
+    end
+
+    if post.nil?
+      render json: { code: '404', message: '存在しない投稿です' }, status: :not_found
+      return
+    end
+    
+    render json: post.json
+  end
+
+  # put /posts/{id}
+  def update
+    if params[:id].nil? || params[:id] =~ /[^0-9]+/
+      render json: { code: '400', message: 'Bad Request' }, status: :bad_request
+      return
+    end
+
+    post=Post.find_by(id:params[:id])
+
+    if post.nil?
+      render json: { code: '404', message: '存在しない投稿です' }, status: :not_found
+      return
+    end
+
+    post.content=params[:content]
+
+    render json: post.json
+  end
+
+  # delete /posts/{id}
+  def destroy
+    if params[:id].nil? || params[:id] =~ /[^0-9]+/
+      render json: { code: '400', message: 'Bad Request' }, status: :bad_request
+      return
+    end
+    post=Post.find_by(id:params[:id])
+
+    if post.nil?
+      render json: { code: '404', message: '存在しない投稿です' }, status: :not_found
+      return
+    end
+
+    unless post.destroy
+      render json: { "code": 500, "message": '投稿の削除に失敗しました' }, status: :internal_server_error
+      return
+    end
+    render json: { "code": 200, "message": '削除しました' }, status: :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/new'
   get 'health_check', to: 'application#health_check'
 
   post 'auth/login'
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
     get 'tags', to: 'users#tags', on: :member
     get 'twitter_profile', to: 'users#twitter_profile', on: :member
   end
+  resources :posts, only: [:show, :update, :destroy]
 end
