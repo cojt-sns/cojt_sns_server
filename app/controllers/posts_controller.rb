32a4a7c06bc5acb51174280f5c46c2fa54121b70
k-usuha <kouta528bado@yahoo.co.jp>
2020-06-13 16:30:47 +0900

    apply rubo-cop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   before_action :authenticate, only: %i(update destroy group create show)
-  
+
   # get /posts
   def index
     params = posts_params
     posts = basic_search(params, Post.all)
 
-    render json: posts.map{ |post| post.json }.to_json
+    render json: posts.map(&:json).to_json
   end
 
   # get /groups/:id/public/posts
@@ -14,28 +14,28 @@ class PostsController < ApplicationController
     params = posts_params
     group = Group.find_by(id: params[:id])
 
-    unless group.present?
-      render json: { "code": 404, "message": 'グループが存在しません' }, status: 404
-      return 
+    if group.blank?
+      render json: { "code": 404, "message": 'グループが存在しません' }, status: :not_found
+      return
     end
 
     unless group.public
       render json: { "code": 403, "message": 'パブリックグループではありません' }, status: :bad_request
-      return 
+      return
     end
 
     posts = basic_search(params, Post.where(group_id: group.id))
-                
-    render json: posts.map{ |post| post.json }.to_json
+
+    render json: posts.map(&:json).to_json
   end
 
   # get /groups/:id/posts
   def group
     params = posts_params
     group = Group.find_by(id: params[:id])
-    unless group.present?
-      render json: { "code": 404, "message": 'グループが存在しません' }, status: 404
-      return 
+    if group.blank?
+      render json: { "code": 404, "message": 'グループが存在しません' }, status: :not_found
+      return
     end
 
     # approvable_ids = User.where.ids
@@ -44,23 +44,23 @@ class PostsController < ApplicationController
     # puts @user.groups.ids
     # puts "\n\n\n\n"
     unless @user.groups.ids.include?(group.id)
-      render json: { "code": 403, "message": 'グループへの権限がありません' }, status: 404
-      return 
+      render json: { "code": 403, "message": 'グループへの権限がありません' }, status: :not_found
+      return
     end
 
-    posts = basic_search(params, Post.where(group_id: params["id"]))
-                
-    render json: posts.map{ |post| post.json }.to_json
+    posts = basic_search(params, Post.where(group_id: params['id']))
+
+    render json: posts.map(&:json).to_json
   end
 
   # post /groups/:id/posts
   def create
     render json: {
       "id": 1,
-      "content": "こんにちは！",
+      "content": 'こんにちは！',
       "user_id": 1,
       "group_id": 1,
-      "created_at": "2020-06-09T09:20:52.220Z"
+      "created_at": '2020-06-09T09:20:52.220Z'
     }
   end
 
@@ -68,10 +68,10 @@ class PostsController < ApplicationController
   def update
     render json: {
       "id": 1,
-      "content": "こんにちは！",
+      "content": 'こんにちは！',
       "user_id": 1,
       "group_id": 1,
-      "created_at": "2020-06-09T09:20:52.220Z"
+      "created_at": '2020-06-09T09:20:52.220Z'
     }
   end
 
@@ -79,10 +79,10 @@ class PostsController < ApplicationController
   def show
     render json: {
       "id": 1,
-      "content": "こんにちは！",
+      "content": 'こんにちは！',
       "user_id": 1,
       "group_id": 1,
-      "created_at": "2020-06-09T09:20:52.220Z"
+      "created_at": '2020-06-09T09:20:52.220Z'
     }
   end
 
@@ -90,10 +90,10 @@ class PostsController < ApplicationController
   def destroy
     render json: {
       "id": 1,
-      "content": "こんにちは！",
+      "content": 'こんにちは！',
       "user_id": 1,
       "group_id": 1,
-      "created_at": "2020-06-09T09:20:52.220Z"
+      "created_at": '2020-06-09T09:20:52.220Z'
     }
   end
 
@@ -104,10 +104,11 @@ class PostsController < ApplicationController
   end
 
   def basic_search(search_params, posts)
-    posts = posts.content_like(search_params["content"])
-                .created_day_range(until_day: search_params["until"], since: search_params["since"])
-                .created_time_range(until_timestamp: search_params["until_timestamp"], since_timestamp: search_params["since_timestamp"])
-                .from_user(search_params["from"])
-                .limit(search_params["max"])
+    posts.content_like(search_params['content'])
+         .created_day_range(until_day: search_params['until'], since: search_params['since'])
+         .created_time_range(until_timestamp: search_params['until_timestamp'],
+                             since_timestamp: search_params['since_timestamp'])
+         .from_user(search_params['from'])
+         .limit(search_params['max'])
   end
 end
