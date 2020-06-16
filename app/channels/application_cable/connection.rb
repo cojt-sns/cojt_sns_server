dcacc74fff131c10f368b0b7434f75fc61463b02
GON <hutabasuzukiryu@live.jp>
2020-06-16 10:50:03 +0900

    wip

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,7 +2,7 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
     def connect
-      self.current_user = reject_unauthorized_connection
+      # self.current_user = reject_unauthorized_connection
     end
   end
 end
