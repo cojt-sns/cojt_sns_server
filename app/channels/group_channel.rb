a29d688e0cf707d53a96887866fb28bf826a1d37
GON <hutabasuzukiryu@live.jp>
2020-06-17 17:07:09 +0900

    fix: lint

--- app/channels/group_channel.rb
+++ app/channels/group_channel.rb
@@ -7,13 +7,9 @@ class GroupChannel < ApplicationCable::Channel
     return stream_from "group_#{params[:id]}" if group.public
 
     auth = AuthenticateToken.find_by(token: params[:token])
-    if !auth.nil? && auth.active && auth.created_at > Time.zone.now - 1.hour
-      return stream_from "group_#{params[:id]}" if auth.user.groups.ids.include?(group.id)
+    return stream_from "group_#{params[:id]}" if !auth.nil? && auth.check && auth.user.groups.ids.include?(group.id)
 
-      reject
-    else
-      reject
-    end
+    reject
   end
 
   def unsubscribed
