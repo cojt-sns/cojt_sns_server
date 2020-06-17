7a540622ca22f55506107601ba50cdaa6c2d146d
GON <hutabasuzukiryu@live.jp>
2020-06-17 17:12:24 +0900

    update: server setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = nil
+  config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://cojt-sns-stg.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
