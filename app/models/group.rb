d720cac6ff5e908e2f4dc6bc7365ce4a971ecbe0
GON <hutabasuzukiryu@live.jp>
2020-07-06 23:46:35 +0900

    add: group validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,6 +7,10 @@ class Group < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  validates :name, format: { with: %r{\A[^\.\#@/\\]+\z} }
+  validate :same_hierarchy_same_name
+  validate :ancestors_same_name
+
   # JSONを返す
   def json
     {
@@ -17,5 +21,17 @@ class Group < ApplicationRecord
       "introduction": introduction,
       "tags": tags.pluck(:id)
     }
+  end
+
+  # validates
+
+  # 兄弟タグに同名タグが存在するか
+  def same_hierarchy_same_name
+    errors.add(:name, '同階層に同名タグが存在します') if siblings.find { |t| t.name == name }.present?
+  end
+
+  # 祖先に同名タグが存在するか
+  def ancestors_same_name
+    errors.add(:name, '祖先に同名タグが存在します') if ancestors.find { |t| t.name == name }.present?
   end
 end
