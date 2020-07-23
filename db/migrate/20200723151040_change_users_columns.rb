1854ca5b592a5f34adb15e1e361ebe3b7ec6c877
GON <hutabasuzukiryu@live.jp>
2020-07-24 02:29:31 +0900

    fix: migration

--- db/migrate/20200723151040_change_users_columns.rb
+++ db/migrate/20200723151040_change_users_columns.rb
@@ -1,13 +1,11 @@
 class ChangeUsersColumns < ActiveRecord::Migration[6.0]
   def up
     change_column :users, :email, :string, null: true
+    remove_index :users, column: [:email]
   end
 
   def down
     change_column :users, :email, :string, null: false
-  end
-
-  def change
-    remove_index :users, column: [:email]
+    add_index :users, column: [:email], unique: true
   end
 end
