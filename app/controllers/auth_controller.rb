ab00348c62f7a746143965176238742c8c6d4469
GON <hutabasuzukiryu@live.jp>
2020-07-24 01:49:55 +0900

    add: twitter auth api

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,5 @@
 class AuthController < ApplicationController
+  include ImageControllerModule
   before_action :authenticate, only: %i(logout user)
 
   # POST /auth/login
@@ -68,5 +69,49 @@ class AuthController < ApplicationController
     end
 
     render json: @user.json
+  end
+
+  # get /auth/:provider/callback
+  def twitter_callback
+    user_data = request.env['omniauth.auth']
+
+    user = User.find_by(oauth_token: user_data[:credentials][:token], oauth_token_secret: user_data[:credentials][:secret])
+
+    if user.nil?
+      user = User.new
+      user.name = user_data[:info][:name]
+      user.bio = user_data[:info][:description]
+      user.oauth_token = user_data[:credentials][:token]
+      user.oauth_token_secret = user_data[:credentials][:secret]
+      user.password = SecureRandom.uuid #passwordは空にできない
+      image =  OpenURI.open_uri(user_data[:info][:image])
+      set_image(user, image, "#{user.id}_#{Time.zone.now}")
+
+      unless user.valid?
+        logger.debug(user.errors.messages)
+        redirect_to ENV['FRONT'] + "/?error=400"
+        return
+      end
+
+      unless user.save
+        redirect_to ENV['FRONT'] + "/?error=500"
+        return
+      end
+    end
+
+    # AuthenticateToken生成
+    auth = AuthenticateToken.new
+    auth.user = user
+    auth.token = SecureRandom.uuid
+    auth.last_access = DateTime.now
+    unless auth.save
+      redirect_to ENV['FRONT'] + "/?error=501"
+      return
+    end
+
+    redirect_to ENV['FRONT'] + "/login?token=#{auth.token}"
+  rescue => e
+    logger.debug(e)
+    redirect_to ENV['FRONT']
   end
 end
